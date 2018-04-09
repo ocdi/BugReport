e94afc30608c08deaad1c5a45146ea79ee2b2055
Trevor Nichols <trevor@ml.net.au>
2018-04-09 12:28:26 +1000

    initial bug report code

---
+++ CommonCode/SomeClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommonCode
+{
+    public class SomeClass
+    {
+        public virtual bool Test() => false;
+
+        string Cause() => "update";
+    }
+}
---
+++ ViewClassThird/Class1.cs
@@ -0,0 +1,16 @@
+﻿using CommonCode;
+using System;
+
+namespace ViewClass
+{
+    public class Class1
+    {
+    }
+}
+namespace ViewClass
+{
+    public class SomeOther : SomeClass
+    {
+        public override bool Test() => false;
+    }
+}
